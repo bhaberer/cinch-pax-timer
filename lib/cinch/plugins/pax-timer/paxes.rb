4e33662f3f2ccb153f14b97d768c38ed78ad3dd6
Alex Mobasher <alex.mobasher@gmail.com>
2017-04-05 01:13:16 -0700

    Update paxes.rb

--- lib/cinch/plugins/pax-timer/paxes.rb
+++ lib/cinch/plugins/pax-timer/paxes.rb
@@ -6,8 +6,8 @@ module Cinch
       PAXES = [
         { type:      'aus',
           name:      'PAX Australia',
-          date:      Time.parse('2017-11-02 08:00:00 +11:00'),
-          estimated: true },
+          date:      Time.parse('2017-10-27 08:00:00 +11:00'),
+          estimated: false },
         { type:      'prime',
           name:      'PAX West',
           date:      Time.parse('2017-09-01 08:00:00 -08:00'),
@@ -18,11 +18,15 @@ module Cinch
           estimated: true },
         { type:      'south',
           name:      'PAX South',
-          date:      Time.parse('2017-01-27 08:00:00 -06:00'),
-          estimated: false },
+          date:      Time.parse('2018-01-26 08:00:00 -06:00'),
+          estimated: true },
         { type:      'east',
           name:      'PAX East',
-          date:      Time.parse('2017-03-10 08:00:00 -05:00'),
+          date:      Time.parse('2018-04-06 08:00:00 -05:00'),
+          estimated: true }
+        { type:      'unplugged',
+          name:      'PAX Unplugged',
+          date:      Time.parse('2017-11-17 08:00:00 -05:00'),
           estimated: false }
       ]
     end
